f6f21f299a0c4b0ffc1791c288b009a7c855aa75
Khalyavin <sts2004@gmail.com>
2022-12-22 19:55:23 +0700

    sj row parsing

--- engine_classes.py
+++ engine_classes.py
@@ -2,6 +2,7 @@ import json
 import requests
 from abc import ABC, abstractmethod
 
+
 class Engine(ABC):
     @abstractmethod
     def get_request(self):
@@ -19,8 +20,7 @@ class HH(Engine):
 
     def _get_vacancies(self, search_word, page):
         print(f'Try to get from hh.ru page number: {page + 1}')
-        responce = requests.get(f'{self.__url}/vacancies?text=java')
-#        responce = requests.get(f'{self.__url}/vacancies?text={search_word}&page={page}')
+        responce = requests.get(f'{self.__url}/vacancies?text={search_word}&page={page}')
         if responce.status_code == 200:
             return responce.json()
 
@@ -41,23 +41,63 @@ class HH(Engine):
 
 
 class SuperJob(Engine):
-    def get_request(self):
-        pass
+    __url = 'https://api.superjob.ru/2.0'
+    __key = 'v3.r.129863982.c889b7015220a7f8a8976eccba85e22445dfe88e.0e1b4bcb3588b68bf13d37b08aedb02499b70256'
+    __per_page = 20
+
+    def _send_request(self, search_word, page):
+        url = f'{self.__url}/vacancies/?page={page}&reyword={search_word}'
+
+        headers = {
+            'X-Api-App-Id': self.__key,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        responce = requests.get(
+            url=url,
+            headers=headers
+        )
+        if responce.status_code == 200:
+            return responce.json()
+
+        return None
+
+
+    def get_request(self, search_word, vacancies_count):
+        page = 0
+        result = []
+        while self.__per_page * page < vacancies_count:
+            tmp_result = self._get_vacancies(search_word, page)
+            if tmp_result:
+                result += tmp_result.get('objects')
+                page += 1
+            else:
+                break
+
+        return result
 
 
 if __name__ == '__main__':
-    hh_engine = HH()
+    # hh_engine = HH()
+    # search_word = 'Python'
+    # vacancies_count = 100
+    #
+    # result = hh_engine.get_request(search_word, vacancies_count)
+    # print(len(result))
+    #
+    # with open('hh_res.json', 'w', encoding='utf-8') as res_file:
+    #     json.dump(result, res_file)
+    #
+    # with open('hh_res.json', 'r', encoding='utf-8') as res_file:
+    #     res_data = json.load(res_file)
+    #     for item in res_data:
+    #         print(f'{item.get("name")}, {item["url"]}, {item.get("salary")}), {item["schedule"].get("name")}, \n    '
+    #               f'{item["snippet"].get("requirement")}, {item["area"].get("name")}')
+
+    sj_engine = SuperJob()
     search_word = 'Python'
     vacancies_count = 100
 
-    result = hh_engine.get_request(search_word, vacancies_count)
-    print(len(result))
+    result = sj_engine.get_request(search_word, vacancies_count)
 
-    with open('hh_res.json', 'w', encoding='utf-8') as res_file:
+    with open('sj_res.json', 'w', encoding='utf-8') as res_file:
         json.dump(result, res_file)
-
-    with open('hh_res.json', 'r', encoding='utf-8') as res_file:
-        res_data = json.load(res_file)
-        for item in res_data:
-            print(f'{item.get("name")}, {item["url"]}, {item.get("salary")}), {item["schedule"].get("name")}, \n    '
-                  f'{item["snippet"].get("requirement")}, {item["area"].get("name")}')
