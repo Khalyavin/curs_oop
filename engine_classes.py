cf5687f6cd0f2cd74ab22edb50304b01a8911d49
Khalyavin <sts2004@gmail.com>
2022-12-23 00:03:53 +0700

    hh & sj parsing ready - output in files

--- engine_classes.py
+++ engine_classes.py
@@ -46,7 +46,8 @@ class SuperJob(Engine):
     __per_page = 20
 
     def _send_request(self, search_word, page):
-        url = f'{self.__url}/vacancies/?page={page}&reyword={search_word}'
+        print(f'Try to get from sj.ru page number: {page + 1}')
+        url = f'{self.__url}/vacancies/?page={page}&keyword={search_word}'
 
         headers = {
             'X-Api-App-Id': self.__key,
@@ -66,7 +67,7 @@ class SuperJob(Engine):
         page = 0
         result = []
         while self.__per_page * page < vacancies_count:
-            tmp_result = self._get_vacancies(search_word, page)
+            tmp_result = self._send_request(search_word, page)
             if tmp_result:
                 result += tmp_result.get('objects')
                 page += 1
@@ -77,27 +78,32 @@ class SuperJob(Engine):
 
 
 if __name__ == '__main__':
-    # hh_engine = HH()
-    # search_word = 'Python'
-    # vacancies_count = 100
-    #
-    # result = hh_engine.get_request(search_word, vacancies_count)
-    # print(len(result))
-    #
-    # with open('hh_res.json', 'w', encoding='utf-8') as res_file:
-    #     json.dump(result, res_file)
-    #
-    # with open('hh_res.json', 'r', encoding='utf-8') as res_file:
-    #     res_data = json.load(res_file)
-    #     for item in res_data:
-    #         print(f'{item.get("name")}, {item["url"]}, {item.get("salary")}), {item["schedule"].get("name")}, \n    '
-    #               f'{item["snippet"].get("requirement")}, {item["area"].get("name")}')
-
-    sj_engine = SuperJob()
+    hh_engine = HH()
     search_word = 'Python'
     vacancies_count = 100
+
+    result = hh_engine.get_request(search_word, vacancies_count)
+
+    with open('hh_res.json', 'w', encoding='utf-8') as res_file:
+        json.dump(result, res_file)
+
+    with open('hh_res.json', 'r', encoding='utf-8') as res_file:
+        res_data = json.load(res_file)
+        for item in res_data:
+            print(f'{item.get("name")}, {item["url"]}, {item.get("salary")}), {item["schedule"].get("name")}, \n    '
+                  f'{item["snippet"].get("requirement")}, {item["area"].get("name")}')
+
+
+    sj_engine = SuperJob()
 
     result = sj_engine.get_request(search_word, vacancies_count)
 
     with open('sj_res.json', 'w', encoding='utf-8') as res_file:
         json.dump(result, res_file)
+
+    with open('sj_res.json', 'r', encoding='utf-8') as res_file:
+        res_data = json.load(res_file)
+        for object in res_data:
+            print(f'{object.get("profession")}, {object.get("link")}, \n    '
+                  f' З/п от {object.get("payment_from")} до {object.get("payment_to")} {object.get("currency")}, '
+                  f'{object["type_of_work"].get("title")}, {object.get("work")}')
