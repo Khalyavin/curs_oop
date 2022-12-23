ee2a56189802113237bccbcf6d486f21e23a3b0e
Khalyavin <sts2004@gmail.com>
2022-12-23 15:52:57 +0700

    sj massive reade for sorting

--- clases.py
+++ clases.py
@@ -3,7 +3,7 @@ from abc import ABC
 
 
 class Vacancy(ABC):
-    __slots__ = ('name', 'link', 'description', 'salary')
+    __slots__ = ('name', 'job', 'link', 'description', 'salary')
 
     def __init__(self, name, job, link, description, salary):
         self.name = name
@@ -11,12 +11,35 @@ class Vacancy(ABC):
         self.link = link
         self.description = description
         self.salary = salary
+        self.usd = 72
+        self.eur = 76
 
     def __str__(self):
         return f'{self.name}, {self.job}, з/п: {self.salary} руб/мес'
 
+    def __gt__(self, other):
+        return self.salary > other
+
+    def __lt__(self, other):
+        return self.salary < other
+
+    def __ge__(self, other):
+        return self.salary >= other
+
+    def __le__(self, other):
+        return self.salary <= other
+
+    def __eq__(self, other):
+        return self.salary == other
+
+    def __ne__(self, other):
+        return self.salary != other
+
+
 
 class CountMixin:
+    def __init__(self, name, job, link, description, salary):
+        super().__init__(name, job, link, description, salary)
 
     #@property
     def get_count_of_vacancy(self, file):
@@ -45,27 +68,59 @@ class HHVacancy(Vacancy, CountMixin):  # add counter mixin
     def __str__(self):
         return 'HH: ' + super().__str__()
 
-    def count_of_vacancies(self):
-        return f'От сервиса hh.ru получено {self.count_of_vacancies("hh_res.json")} вакансий.'
+    # def count_of_vacancies(self):
+    #     return f'От сервиса hh.ru получено {self.get_count_of_vacancies("hh_res.json")} вакансий.'
 
 
 class SJVacancy(Vacancy, CountMixin):  # add counter mixin
     """ SuperJob Vacancy """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name, job, link, description, salary):
+        super().__init__(name, job, link, description, salary)
 
     def __str__(self):
-        return 'HH: ' + super().__str__()
+        return 'SJ: ' + super().__str__()
 
     def count_of_vacancies(self):
-        return f'От сервиса sj.ru получено {self.count_of_vacancies("sj_res.json")} вакансий.'
+        return f'От сервиса sj.ru получено {self.get_count_of_vacancies("sj_res.json")} вакансий.'
 
 
-def sorting(vacancies):
-    """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods) """
-    pass
+def sorting(self):
+    """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods)
+     Сортирует sj_res.json в sj_res_sort.json"""
+
+    data = []
+    fp = open('sj_res.json', 'r', encoding='utf-8')
+    tmp_data = json.load(fp)
+    fp.close()
+
+    for object in tmp_data:
+        tmp_name = object["client"].get("title")
+        tmp_job = object.get("profession")
+        tmp_link = object.get("link")
+        tmp_description = object.get("work")
+        tmp_salary_from = object.get("payment_from")
+        tmp_salary_to = object.get("payment_to")
+        tmp_salary_curr = object.get("currency")
+
+        tmp_salary = 0
+        if tmp_salary_from:
+            tmp_salary = tmp_salary_from
+        else:
+            tmp_salary = tmp_salary_to
+
+        if tmp_salary_curr == 'usd': tmp_salary *= self.usd
+        if tmp_salary_curr == 'eur': tmp_salary *= self.eur
+
+        sj_vac = SJVacancy(tmp_name, tmp_job, tmp_link, tmp_description, tmp_salary)
+        data.append(sj_vac)
+
+    return data
 
 
 def get_top(vacancies, top_count):
     """ Должен возвращать {top_count} записей из вакансий по зарплате (iter, next magic methods) """
     pass
+
+
+if __name__ == '__main__':
+    sorting('sj_res.json')
