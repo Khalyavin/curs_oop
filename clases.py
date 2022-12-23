b5ee6e6db1530b01e8f7761efbc103044251385c
Khalyavin <sts2004@gmail.com>
2022-12-23 11:00:18 +0700

    classes + mixin ready

--- clases.py
+++ clases.py
@@ -1,3 +1,4 @@
+import json
 from abc import ABC
 
 
@@ -17,16 +18,26 @@ class Vacancy(ABC):
 
 class CountMixin:
 
-    @property
-    def get_count_of_vacancy(self):
+    #@property
+    def get_count_of_vacancy(self, file):
         """
         Вернуть количество вакансий от текущего сервиса.
         Получать количество необходимо динамически из файла.
         """
-        pass
+        self.data_file = file
+        data = []
+        try:
+            fp = open(self.data_file, 'r', encoding='utf-8')
+        except FileNotFoundError:
+            print(f'Скачайте вакансии в файл {self.data_file}')
+        else:
+            data = json.load(fp)
+
+        fp.close()
+        return len(data)
 
 
-class HHVacancy(Vacancy):  # add counter mixin
+class HHVacancy(Vacancy, CountMixin):  # add counter mixin
     """ HeadHunter Vacancy """
     def __init__(self):
         super().__init__()
@@ -34,14 +45,20 @@ class HHVacancy(Vacancy):  # add counter mixin
     def __str__(self):
         return 'HH: ' + super().__str__()
 
+    def count_of_vacancies(self):
+        return f'От сервиса hh.ru получено {self.count_of_vacancies("hh_res.json")} вакансий.'
 
-class SJVacancy(Vacancy):  # add counter mixin
+
+class SJVacancy(Vacancy, CountMixin):  # add counter mixin
     """ SuperJob Vacancy """
     def __init__(self):
         super().__init__()
 
     def __str__(self):
         return 'HH: ' + super().__str__()
+
+    def count_of_vacancies(self):
+        return f'От сервиса sj.ru получено {self.count_of_vacancies("sj_res.json")} вакансий.'
 
 
 def sorting(vacancies):
