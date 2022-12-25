eaa1b9d2b02e83465f2eeb2c7ab94f2c72ba91cc
Khalyavin <sts2004@gmail.com>
2022-12-25 11:23:22 +0700

    sj + hh parsed data sorted and save to file

--- clases.py
+++ clases.py
@@ -3,6 +3,9 @@ from abc import ABC
 
 
 class Vacancy(ABC):
+    usd = 72
+    eur = 76
+
     __slots__ = ('name', 'job', 'link', 'description', 'salary')
 
     def __init__(self, name, job, link, description, salary):
@@ -11,11 +14,14 @@ class Vacancy(ABC):
         self.link = link
         self.description = description
         self.salary = salary
-        self.usd = 72
-        self.eur = 76
 
     def __str__(self):
         return f'{self.name}, {self.job}, з/п: {self.salary} руб/мес'
+
+    def get_raw_data(self):
+        return {
+            'name': self.name, 'job': self.job, 'lnk': self.link, 'description': self.description, 'salary': self.salary
+        }
 
     def __gt__(self, other):
         return self.salary > other
@@ -77,20 +83,16 @@ class SJVacancy(Vacancy, CountMixin):  # add counter mixin
     def __str__(self):
         return 'SJ: ' + super().__str__()
 
-    def get_raw_data(self):
-        return {
-            'name': self.name, 'job': self.job, 'lnk': self.link, 'description': self.description, 'salary': self.salary
-        }
+def sorting(file):
+    """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods)
+     Сортирует sj_res.json в sj_sort.json"""
 
-    def sorting(self):
-        """ Должен сортировать любой список вакансий по ежемесячной оплате (gt, lt magic methods)
-         Сортирует sj_res.json в sj_sort.json"""
+    data = []
+    fp = open(file, 'r', encoding='utf-8')
+    tmp_data = json.load(fp)
+    fp.close()
 
-        data = []
-        fp = open('sj_res.json', 'r', encoding='utf-8')
-        tmp_data = json.load(fp)
-        fp.close()
-
+    if file == 'sj_res.json':
         for object in tmp_data:
             tmp_name = object["client"].get("title")
             tmp_job = object.get("profession")
@@ -106,18 +108,46 @@ class SJVacancy(Vacancy, CountMixin):  # add counter mixin
             else:
                 tmp_salary = tmp_salary_to
 
-            if tmp_salary_curr == 'usd': tmp_salary *= self.usd
-            if tmp_salary_curr == 'eur': tmp_salary *= self.eur
+            if tmp_salary_curr == 'USD': tmp_salary *= Vacancy.usd
+            if tmp_salary_curr == 'EUR': tmp_salary *= Vacancy.eur
 
             # Данные для десериализации готовы
             sj_vac = SJVacancy(tmp_name, tmp_job, tmp_link, tmp_description, tmp_salary)
             data.append(sj_vac)
 
-        data.sort(reverse=True)
+    elif file == 'hh_res.json':
+        for item in tmp_data:
+            tmp_name = item["employer"].get("name")
+            tmp_job = item.get("name")
+            tmp_link = item.get("alternate_url")
+            tmp_description = item["snippet"].get("requirement")
+            if item.get("salary"):
+                tmp_salary_from = item["salary"].get("from")
+                tmp_salary_to = item["salary"].get("to")
+                tmp_salary_curr = item["salary"].get("currency")
 
-        fp = open('sj_sort.json', 'w', encoding='utf-8')
-        json.dump(data, fp, indent=2, ensure_ascii=False, default=lambda o: o.get_raw_data())
-        fp.close()
+                tmp_salary = 0
+                if tmp_salary_from:
+                    tmp_salary = tmp_salary_from
+                else:
+                    tmp_salary = tmp_salary_to
+
+                if tmp_salary_curr == 'USD': tmp_salary *= Vacancy.usd
+                if tmp_salary_curr == 'EUR': tmp_salary *= Vacancy.eur
+            else:
+                tmp_salary = 0
+
+            # Данные для десериализации готовы
+            hh_vac = HHVacancy(tmp_name, tmp_job, tmp_link, tmp_description, tmp_salary)
+            data.append(hh_vac)
+
+    data.sort(reverse=True)
+
+    f_name = file.replace('res', 'sort')
+
+    fp = open(f_name, 'w', encoding='utf-8')
+    json.dump(data, fp, indent=2, default=lambda o: o.get_raw_data())
+    fp.close()
 
 
 
@@ -127,4 +157,5 @@ def get_top(vacancies, top_count):
 
 
 if __name__ == '__main__':
-    SJVacancy.sorting('sj_res.json')
+    sorting('sj_res.json')
+    sorting('hh_res.json')
